5ff95046b1a254b69bc85c47be8d7f17d9f8111b
Jalen <jalenmoore@unomaha.edu>
2024-07-29 21:45:16 -0500

    Bug fixes. (#1)
    * renamed package -> dependency
    
    * added proper checking for dependencies that are already cloned.
    
    ---------
    
    Co-authored-by: Jalen Moore <jalenmoore@Jalens-MacBook-Pro.local>

--- hpkg.asd
+++ hpkg.asd
@@ -1,9 +1,21 @@
-(in-package :asdf-user)
-
-(defsystem "hpkg"
+(asdf:defsystem #:hpkg
   :description "hpkg: a hacky package fetcher using the git shell client."
   :version "0.0.1"
   :author "Jalen Moore <moo.jalen@gmail.com>"
   :license "Public Domain"
-  :components ((:file "package")
-               (:file "main" :depends-on ("package"))))
+  :pathname "src"
+  :components
+  ((:file "package")
+   (:file "main" :depends-on ("package"))))
+
+;;; Currently buggy. Testing doesn't work correctly.
+;;; I have to load the system *here*, not during a test.
+(asdf:defsystem #:hpkg/test
+  :description "Tests for hpkg."
+  :author "Jalen Moore <moo.jalen@gmail.com>"
+  :license "Public Domain"
+  :depends-on (:hpkg)
+  :pathname "test"
+  :components
+  ((:file "package")
+   (:file "test" :depends-on ("package"))))
--- src/main.lisp
+++ src/main.lisp
@@ -6,24 +6,29 @@
 (defparameter *default-destination* (concatenate 'string *home*
                                                "/.local/share/common-lisp/source/"))
 
-(defun get-pkg-dest (parent package)
-  (concatenate 'string parent (string package)))
+(defun is-dependency-installed (dependency &key (dest *default-destination*))
+  (let ((result (uiop:run-program `("find" ,dest "-name" ,(string (car dependency)))
+                                  :output :string
+                                  :ignore-error-status t)))
+    (not (string= result ""))))
 
-(defun get-remote (package &key (dest *default-destination*) debug)
+(defun get-dependency-dest (parent dependency)
+  (concatenate 'string parent (string dependency)))
+
+(defun get-remote (dependency &key (dest *default-destination*) debug)
   "Clone the given remote asdf system. Clones to '~/.local/share/common-lisp/source/' by default."
-  (multiple-value-bind (output error status)
-      (uiop:run-program `("git" "clone" ,@(cdr package) ,(get-pkg-dest dest (car package)))
-                        :output :string
-                        :ignore-error-status t)
-    (if debug
-        (cond ((= status 0)
-               (format t "Cloned: ~a.~%" (car package)))
-              ((= status 128)
-               (format t "Package exists: ~a.~%" (car package)))))
-    (values output error status)))
+  (let ((dpd-name (string (car dependency))))
+    (if (not (is-dependency-installed dependency))
+        (progn
+          (uiop:run-program `("git" "clone" ,@(cdr dependency) ,(get-dependency-dest dest (car dependency)))
+                            :output :string
+                            :ignore-error-status t)
+          (format t "Cloned: ~a...~%" dpd-name))
+        (if debug
+            (format t "Skipped: ~a...~%" dpd-name)))))
 
-(defmacro batch-remote (&rest package-list)
+(defmacro batch-remote (&rest dependency-list)
   "Batch a list of remote asdf system for cloning. Cloned sequentially."
   `(progn
-     ,@(loop :for pkg :in package-list
-             :collect `(get-remote ',pkg :debug t))))
+     ,@(loop :for dpd :in dependency-list
+             :collect `(get-remote ',dpd :debug t))))
